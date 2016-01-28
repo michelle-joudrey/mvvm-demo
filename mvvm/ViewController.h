18cf8280b41adb15af28d5a508e9a42f56f58610
Michelle Joudrey <michelle-joudrey@users.noreply.github.com>
2016-01-27 20:20:07 -0500

    Initial Commit

---
+++ mvvm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mvvm
+//
+//  Created by Michelle Joudrey on 1/27/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mvvm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mvvm
+//
+//  Created by Michelle Joudrey on 1/27/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
